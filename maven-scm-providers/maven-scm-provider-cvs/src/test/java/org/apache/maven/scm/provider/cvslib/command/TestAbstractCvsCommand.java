27a0b1fbac51a604beb52d49db0a7da675023889
Trygve Laugstoul <trygvis@apache.org>
2004-10-26 12:43:17 +0000

    o Updating the license.
    git-svn-id: https://svn.apache.org/repos/asf/maven/scm/trunk@162174 13f79535-47bb-0310-9956-ffa450edef68

--- maven-scm-providers/maven-scm-provider-cvs/src/test/java/org/apache/maven/scm/provider/cvslib/command/TestAbstractCvsCommand.java
+++ maven-scm-providers/maven-scm-provider-cvs/src/test/java/org/apache/maven/scm/provider/cvslib/command/TestAbstractCvsCommand.java
@@ -1,7 +1,7 @@
 package org.apache.maven.scm.provider.cvslib.command;
 
-/* ====================================================================
- * Copyright 2003-2004 The Apache Software Foundation.
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@ package org.apache.maven.scm.provider.cvslib.command;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ====================================================================
  */
 
 import org.apache.maven.scm.ScmException;
@@ -35,12 +34,12 @@ public class TestAbstractCvsCommand
     {
         return null;
     }
-    
+
     public String getName()
     {
         return null;
     }
-    
+
     public String getDisplayName()
     {
         return null;
