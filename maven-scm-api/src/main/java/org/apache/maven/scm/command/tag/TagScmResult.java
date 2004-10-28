b7cc4a61483f05f307c4afbb8a6bc41fad635d79
Trygve Laugstoul <trygvis@apache.org>
2004-10-28 19:38:41 +0000

    o Adding missing file.
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/scm/trunk@162179 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-scm-api/src/main/java/org/apache/maven/scm/command/tag/TagScmResult.java
@@ -0,0 +1,49 @@
+package org.apache.maven.scm.command.tag;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.List;
+import java.io.File;
+
+import org.apache.maven.scm.ScmResult;
+
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id$
+ */
+public class TagScmResult
+    extends ScmResult
+{
+    private List taggedFiles;
+
+    public TagScmResult( String providerMessage, String commandOutput, boolean success )
+    {
+        super( providerMessage, commandOutput, success );
+    }
+
+    public TagScmResult( List taggedFiles )
+    {
+        super( null, null, true );
+
+        this.taggedFiles = taggedFiles;
+    }
+
+    public List getTaggedFiles()
+    {
+        return taggedFiles;
+    }
+}
