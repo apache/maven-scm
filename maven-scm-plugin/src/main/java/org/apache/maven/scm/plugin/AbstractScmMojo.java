e45a79618f8f3f3294783eb21d896c4ac7928d3c
Emmanuel Venisse <evenisse@apache.org>
2005-10-19 15:54:43 +0000

    PR: SCM-62 Submitted by Dan Tran Don't ignore starteam's username and password configurations
    git-svn-id: https://svn.apache.org/repos/asf/maven/scm/trunk@326588 13f79535-47bb-0310-9956-ffa450edef68

--- maven-scm-plugin/src/main/java/org/apache/maven/scm/plugin/AbstractScmMojo.java
+++ maven-scm-plugin/src/main/java/org/apache/maven/scm/plugin/AbstractScmMojo.java
@@ -22,6 +22,7 @@ import org.apache.maven.scm.ScmException;
 import org.apache.maven.scm.ScmFileSet;
 import org.apache.maven.scm.ScmResult;
 import org.apache.maven.scm.manager.ScmManager;
+import org.apache.maven.scm.provider.starteam.repository.StarteamScmProviderRepository;
 import org.apache.maven.scm.provider.svn.repository.SvnScmProviderRepository;
 import org.apache.maven.scm.repository.ScmRepository;
 
@@ -150,6 +151,21 @@ public abstract class AbstractScmMojo
                     svnRepo.setTagBase( tagBase );
                 }
             }
+            
+            if ( repository.getProvider().equals( "starteam" ) )
+            {
+                StarteamScmProviderRepository starteamRepo = (StarteamScmProviderRepository) repository.getProviderRepository();
+
+                if ( username != null && username.length() > 0 )
+                {
+                    starteamRepo.setUser( username );
+                }
+                if ( password != null && password.length() > 0 )
+                {
+                    starteamRepo.setPassword( password );
+                }
+            }
+            
         }
         catch ( Exception e )
         {
