fb1c74973adcbd3ef79d5f082d8089dc25f2944b
Emmanuel Venisse <evenisse@apache.org>
2005-11-21 18:00:14 +0000

    Add license
    git-svn-id: https://svn.apache.org/repos/asf/maven/scm/trunk@345939 13f79535-47bb-0310-9956-ffa450edef68

--- maven-scm-providers/maven-scm-provider-clearcase/src/test/java/org/apache/maven/scm/provider/clearcase/command/add/ClearCaseAddCommandTest.java
+++ maven-scm-providers/maven-scm-provider-clearcase/src/test/java/org/apache/maven/scm/provider/clearcase/command/add/ClearCaseAddCommandTest.java
@@ -1,5 +1,21 @@
 package org.apache.maven.scm.provider.clearcase.command.add;
 
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.apache.maven.scm.ScmFileSet;
 import org.apache.maven.scm.ScmTestCase;
 import org.codehaus.plexus.util.cli.Commandline;
@@ -7,7 +23,7 @@ import org.codehaus.plexus.util.cli.Commandline;
 import java.io.File;
 
 /**
- * 
+ * @author <a href="mailto:wim.deblauwe@gmail.com">Wim Deblauwe</a>
  */
 public class ClearCaseAddCommandTest extends ScmTestCase
 {
