3d03c4ff869a8710f1de085b283c6260d1e3a317
Emmanuel Venisse <evenisse@apache.org>
2006-02-03 14:21:03 +0000

    [SCM-151][SCM-136] Allow to define global cvs options
    git-svn-id: https://svn.apache.org/repos/asf/maven/scm/trunk@374674 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-scm-providers/maven-scm-provider-cvs/src/main/java/org/apache/maven/scm/provider/cvslib/util/CvsUtil.java
@@ -0,0 +1,66 @@
+package org.apache.maven.scm.provider.cvslib.util;
+
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.apache.maven.scm.providers.cvslib.settings.Settings;
+import org.apache.maven.scm.providers.cvslib.settings.io.xpp3.CvsXpp3Reader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author <a href="mailto:evenisse@apache.org">Emmanuel Venisse</a>
+ * @version $Id$
+ */
+public class CvsUtil
+{
+    private CvsUtil()
+    {
+    }
+
+    public static Settings getSettings()
+    {
+        File scmUserDir = new File( System.getProperty( "user.dir" ), ".scm" );
+        File settingsFile = new File( scmUserDir, "cvs-settings.xml" );
+
+        if ( settingsFile.exists() )
+        {
+            CvsXpp3Reader reader = new CvsXpp3Reader();
+            try
+            {
+                return reader.read( new FileReader( settingsFile ) );
+            }
+            catch ( FileNotFoundException e )
+            {
+            }
+            catch ( IOException e )
+            {
+            }
+            catch ( XmlPullParserException e )
+            {
+                String message = settingsFile.getAbsolutePath() + " isn't well formed. SKIPPED." + e.getMessage();
+
+                System.out.println( message );
+            }
+        }
+
+        return new Settings();
+    }
+}
