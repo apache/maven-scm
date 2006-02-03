df17a55916a4f8f9b6cecda7ff9bfd32c36a796a
Emmanuel Venisse <evenisse@apache.org>
2006-02-03 14:21:03 +0000

    [SCM-151][SCM-136] Allow to define global cvs options
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/scm/trunk@374674 13f79535-47bb-0310-9956-ffa450edef68

--- maven-scm-providers/maven-scm-provider-cvs/src/main/java/org/apache/maven/scm/provider/cvslib/command/CvsCommandUtils.java
+++ maven-scm-providers/maven-scm-provider-cvs/src/main/java/org/apache/maven/scm/provider/cvslib/command/CvsCommandUtils.java
@@ -18,7 +18,13 @@ package org.apache.maven.scm.provider.cvslib.command;
 
 import org.apache.maven.scm.ScmFileSet;
 import org.apache.maven.scm.provider.cvslib.repository.CvsScmProviderRepository;
+import org.apache.maven.scm.provider.cvslib.util.CvsUtil;
+import org.apache.maven.scm.providers.cvslib.settings.Settings;
+import org.codehaus.plexus.util.StringUtils;
 import org.codehaus.plexus.util.cli.Commandline;
+
+import java.io.File;
+import java.util.Enumeration;
 
 /**
  * @author <a href="mailto:evenisse@apache.org">Emmanuel Venisse</a>
@@ -38,18 +44,57 @@ public class CvsCommandUtils
     public static Commandline getBaseCommand( String commandName, CvsScmProviderRepository repo, ScmFileSet fileSet,
                                               String options )
     {
+        Settings settings = CvsUtil.getSettings();
+
         Commandline cl = new Commandline();
 
         cl.setExecutable( "cvs" );
 
         cl.setWorkingDirectory( fileSet.getBasedir().getAbsolutePath() );
 
-        if ( !System.getProperty( "maven.scm.cvs.use_compression", "true" ).equals( "false" ) )
+        if ( settings.getCompressionLevel() > 0 )
+        {
+            cl.createArgument().setValue( "-z3" );
+        }
+        else if ( !System.getProperty( "maven.scm.cvs.use_compression", "true" ).equals( "false" ) )
         {
             cl.createArgument().setValue( "-z3" );
         }
 
-        cl.createArgument().setValue( "-f" ); // don't use ~/.cvsrc
+        if ( !settings.isUseCvsrc() )
+        {
+            cl.createArgument().setValue( "-f" ); // don't use ~/.cvsrc
+        }
+
+        if ( settings.isTraceCvsCommand() )
+        {
+            cl.createArgument().setValue( "-t" );
+        }
+
+        if ( !StringUtils.isEmpty( settings.getTemporaryFilesDirectory() ) )
+        {
+            File tempDir = new File( settings.getTemporaryFilesDirectory() );
+
+            if ( !tempDir.exists() )
+            {
+                tempDir.mkdirs();
+            }
+
+            cl.createArgument().setValue( "-T" );
+
+            cl.createArgument().setValue( tempDir.getAbsolutePath() );
+        }
+
+        if ( settings.getCvsVariables().size() > 0 )
+        {
+            for ( Enumeration e = settings.getCvsVariables().propertyNames(); e.hasMoreElements(); )
+            {
+                String key = (String) e.nextElement();
+                String value = settings.getCvsVariables().getProperty( key );
+                cl.createArgument().setValue( "-s" );
+                cl.createArgument().setValue( key + "=" + value );
+            }
+        }
 
         cl.createArgument().setValue( "-d" );
 
