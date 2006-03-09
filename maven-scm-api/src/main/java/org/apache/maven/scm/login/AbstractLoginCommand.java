04ac498b8ae1789d326e0763f5a6b159118bd2c7
Emmanuel Venisse <evenisse@apache.org>
2006-03-09 08:44:00 +0000

    Re-add login package. It was removed instead deprecated.
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/scm/trunk@384463 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-scm-api/src/main/java/org/apache/maven/scm/login/AbstractLoginCommand.java
@@ -0,0 +1,47 @@
+package org.apache.maven.scm.login;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.scm.CommandParameters;
+import org.apache.maven.scm.ScmException;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.ScmResult;
+import org.apache.maven.scm.command.AbstractCommand;
+import org.apache.maven.scm.provider.ScmProviderRepository;
+
+/**
+ * @author <a href="mailto:evenisse@apache.org">Emmanuel Venisse</a>
+ * @version $Id$
+ * @deprecated
+ */
+public abstract class AbstractLoginCommand
+    extends AbstractCommand
+{
+    public abstract LoginScmResult executeLoginCommand( ScmProviderRepository repository, ScmFileSet fileSet,
+                                                        CommandParameters parameters )
+        throws ScmException;
+
+    /**
+     * @see org.apache.maven.scm.command.AbstractCommand#executeCommand(org.apache.maven.scm.provider.ScmProviderRepository, org.apache.maven.scm.ScmFileSet, org.apache.maven.scm.CommandParameters)
+     */
+    protected ScmResult executeCommand( ScmProviderRepository repository, ScmFileSet fileSet,
+                                        CommandParameters parameters )
+        throws ScmException
+    {
+        return executeLoginCommand( repository, fileSet, parameters );
+    }
+}
