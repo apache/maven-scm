425a42b0dbc1d5167ec47ae46ed9c6ed87bb59e4
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-05-05 03:20:31 +0000

    [SCM-183] Add list method to ScmProvider
    git-svn-id: https://svn.apache.org/repos/asf/maven/scm/trunk@399925 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-scm-providers/maven-scm-providers-svn/maven-scm-provider-svntest/src/main/java/org/apache/maven/scm/provider/svn/command/list/SvnListCommandTckTest.java
@@ -0,0 +1,44 @@
+package org.apache.maven.scm.provider.svn.command.list;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.scm.provider.svn.SvnScmTestUtils;
+import org.apache.maven.scm.tck.command.list.ListCommandTckTest;
+
+/**
+ * Test for <code>list<code> command
+ * 
+ * @author <a href="mailto:carlos@apache.org">Carlos Sanchez</a>
+ * @version $Id$
+ */
+public class SvnListCommandTckTest
+    extends ListCommandTckTest
+{
+    public String getScmUrl()
+        throws Exception
+    {
+        return SvnScmTestUtils.getScmUrl( new File( getRepositoryRoot(), "trunk" ) );
+    }
+
+    public void initRepo()
+        throws Exception
+    {
+        SvnScmTestUtils.initializeRepository( getRepositoryRoot() );
+    }
+}
