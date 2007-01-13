0fce61b0c504f5b936ce119b76ad7cc53b7780fd
Kenney Westerhof <kenney@apache.org>
2007-01-13 00:22:01 +0000

    add list command implementation
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/scm/trunk@495794 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-scm-providers/maven-scm-providers-cvs/maven-scm-provider-cvs-commons/src/main/java/org/apache/maven/scm/provider/cvslib/command/list/AbstractCvsListCommand.java
@@ -0,0 +1,64 @@
+package org.apache.maven.scm.provider.cvslib.command.list;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.scm.ScmException;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.command.list.AbstractListCommand;
+import org.apache.maven.scm.command.list.ListScmResult;
+import org.apache.maven.scm.provider.ScmProviderRepository;
+import org.apache.maven.scm.provider.cvslib.command.CvsCommand;
+import org.apache.maven.scm.provider.cvslib.command.CvsCommandUtils;
+import org.apache.maven.scm.provider.cvslib.repository.CvsScmProviderRepository;
+import org.codehaus.plexus.util.cli.Commandline;
+
+/**
+ *
+ * @author <a href="mailto:kenney@apache.org">Kenney Westerhof</a>
+ *
+ */
+public abstract class AbstractCvsListCommand
+    extends AbstractListCommand
+    implements CvsCommand
+{
+    protected ListScmResult executeListCommand( ScmProviderRepository repo, ScmFileSet fileSet,
+                                                boolean recursive )
+        throws ScmException
+    {
+        CvsScmProviderRepository repository = (CvsScmProviderRepository) repo;
+
+        Commandline cl = CvsCommandUtils.getBaseCommand( "rls", repository, fileSet, "-n" );
+
+        cl.createArgument().setValue( "-d" );
+
+        if ( recursive  )
+        {
+            cl.createArgument().setValue( "-R" );
+        }
+
+        getLogger().info( "Executing: " + cl );
+        getLogger().info( "Working directory: " + cl.getWorkingDirectory().getAbsolutePath() );
+
+        return executeCvsCommand( cl );
+    }
+
+    protected abstract ListScmResult executeCvsCommand( Commandline cl )
+        throws ScmException;
+}
