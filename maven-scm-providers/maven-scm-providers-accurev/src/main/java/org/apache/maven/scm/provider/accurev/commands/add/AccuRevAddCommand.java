15dafee174b47b5bfb62527123dcff07aebba401
Jason van Zyl <jvanzyl@apache.org>
2008-05-05 19:49:57 +0000

    o adding an Accurev provider donated by the Accurev folks Submitted by: Ivan Luzyanin CCLA is now on file
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/scm/trunk@653572 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-scm-providers/maven-scm-providers-accurev/src/main/java/org/apache/maven/scm/provider/accurev/commands/add/AccuRevAddCommand.java
@@ -0,0 +1,86 @@
+package org.apache.maven.scm.provider.accurev.commands.add;
+
+/*
+ * Copyright 2008 AccuRev Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.scm.command.add.AbstractAddCommand;
+import org.apache.maven.scm.command.add.AddScmResult;
+import org.apache.maven.scm.ScmResult;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.ScmException;
+import org.apache.maven.scm.repository.ScmRepositoryException;
+import org.apache.maven.scm.provider.ScmProviderRepository;
+import org.apache.maven.scm.provider.accurev.AccuRevScmProviderRepository;
+import org.apache.maven.scm.provider.accurev.AccuRevScmProvider;
+import org.codehaus.plexus.util.cli.Commandline;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
+import org.codehaus.plexus.util.cli.CommandLineException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+
+public class AccuRevAddCommand extends AbstractAddCommand
+{
+    private final String _accurevExecutable;
+
+    public AccuRevAddCommand( String accurevExecutable )
+    {
+        _accurevExecutable = accurevExecutable;
+    }
+
+    protected ScmResult executeAddCommand( ScmProviderRepository repository, ScmFileSet fileSet,
+                                           String message, boolean binary ) throws ScmException
+    {
+        Commandline cl = new Commandline();
+        cl.setExecutable( _accurevExecutable );
+        cl.setWorkingDirectory( fileSet.getBasedir().getPath() );
+        cl.addArguments( new String[]{"add"} );
+        ArrayList params = new ArrayList();
+        AccuRevScmProvider.appendHostToParamsIfNeeded( (AccuRevScmProviderRepository) repository, params );
+        cl.addArguments( (String[]) params.toArray( new String[params.size()] ) );
+
+        cl.addArguments( makeFileArgs( fileSet.getFileList() ) );
+
+        final List filesAdded = new ArrayList();
+        final CommandLineUtils.StringStreamConsumer stdout = new CommandLineUtils.StringStreamConsumer();
+        try
+        {
+            if ( 0 != CommandLineUtils.executeCommandLine( cl, new AddCommandStreamConsumer( stdout, filesAdded ),
+                stdout ) )
+            {
+                return new AddScmResult( cl.toString(), null, stdout.getOutput(), false );
+            }
+        }
+        catch ( CommandLineException e )
+        {
+            throw new ScmRepositoryException( "Cannot exeucute add command", e );
+        }
+        return new AddScmResult( cl.toString(), filesAdded );
+    }
+
+    private String[] makeFileArgs( List/*File*/ fileList )
+    {
+        ArrayList res = new ArrayList();
+        for ( int i = 0; i < fileList.size(); i++ )
+        {
+            File file = (File) fileList.get( i );
+            res.add( file.getAbsolutePath() );
+        }
+        return (String[]) res.toArray( new String[res.size()] );
+    }
+
+}
---
+++ maven-scm-providers/maven-scm-providers-accurev/src/main/java/org/apache/maven/scm/provider/accurev/commands/checkout/AccuRevCheckOutWorkspaceCommand.java
@@ -0,0 +1,150 @@
+package org.apache.maven.scm.provider.accurev.commands.checkout;
+
+/*
+ * Copyright 2008 AccuRev Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.scm.ScmException;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.ScmVersion;
+import org.apache.maven.scm.command.checkout.AbstractCheckOutCommand;
+import org.apache.maven.scm.command.checkout.CheckOutScmResult;
+import org.apache.maven.scm.provider.ScmProviderRepository;
+import org.apache.maven.scm.provider.accurev.AccuRevScmProviderRepository;
+import org.apache.maven.scm.provider.accurev.AccuRevScmProvider;
+import org.apache.maven.scm.util.AbstractConsumer;
+import org.codehaus.plexus.util.cli.CommandLineException;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
+import org.codehaus.plexus.util.cli.Commandline;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AccuRevCheckOutWorkspaceCommand extends AbstractCheckOutCommand implements BaseAccuRevCheckOutCommand
+{
+    private String accuRevExecutable;
+
+    public AccuRevCheckOutWorkspaceCommand( String executable )
+    {
+        this.accuRevExecutable = executable;
+    }
+
+    public String getMethodName()
+    {
+        return "mkws";
+    }
+
+    protected CheckOutScmResult executeCheckOutCommand( ScmProviderRepository repository, ScmFileSet fileSet,
+                                                        ScmVersion version ) throws ScmException
+    {
+        try
+        {
+            final CommandLineUtils.StringStreamConsumer stdout = new CommandLineUtils.StringStreamConsumer();
+
+            //Create checkout folder if missing
+            if ( !fileSet.getBasedir().exists() )
+            {
+                fileSet.getBasedir().mkdirs();
+            }
+
+            //Make new workspace
+            AccuRevScmProviderRepository accurevRepository = (AccuRevScmProviderRepository) repository;
+            Commandline makeCommandline = createMakeWorkspaceCommandLine( accurevRepository, fileSet, version );
+
+            int exitCode = CommandLineUtils.executeCommandLine( makeCommandline, stdout, stdout );
+
+            if ( exitCode != 0 )
+            {
+                return new CheckOutScmResult( makeCommandline.toString(),
+                    "The AccuRev command failed. Cannot create workspace: " +
+                        stdout.getOutput(), stdout.getOutput(), false );
+            }
+            //Update the workspace
+            Commandline updateCommandline = createUpdateWorkspaceCommand( accurevRepository, fileSet, version );
+
+            final ArrayList checkedFiles = new ArrayList();
+            exitCode = CommandLineUtils.executeCommandLine( updateCommandline, new AbstractConsumer( getLogger() )
+            {
+                private Pattern pattern = Pattern.compile( "Updating element (.*)" );
+
+                public void consumeLine( String line )
+                {
+                    //Consume line to stdout consumer
+                    stdout.consumeLine( line );
+                    //Check if the line is matching pattern
+                    Matcher m = pattern.matcher( line );
+                    if ( m.matches() )
+                    {
+                        checkedFiles.add( m.group( 1 ) );
+                    }
+                }
+            }, stdout );
+            //Check if the update command successeed
+            if ( exitCode != 0 )
+            {
+                return new CheckOutScmResult( updateCommandline.toString(),
+                    "The AccuRev command failed. Cannot update workspace", stdout.getOutput(), false );
+            }
+            return new CheckOutScmResult( makeCommandline.toString() + " & " + updateCommandline.toString(),
+                checkedFiles );
+        }
+        catch ( CommandLineException e )
+        {
+            throw new ScmException( "internal error" );
+        }
+    }
+
+    protected Commandline createUpdateWorkspaceCommand( AccuRevScmProviderRepository repository,
+                                                        ScmFileSet fileSet, ScmVersion version )
+    {
+        //TODO Implement support of fileSet, version if applyable
+        Commandline commandline = new Commandline();
+        commandline.setExecutable( this.accuRevExecutable );
+        ArrayList params = new ArrayList();
+        //Append command name
+        params.add( "update" );
+        //Append host if needed
+        AccuRevScmProvider.appendHostToParamsIfNeeded( repository, params );
+        //Append command parameters
+        params.add( "-r" );
+        params.add( repository.getWorkspaceName() );
+        //Set command parameters
+        commandline.addArguments( (String[]) params.toArray( new String[params.size()] ) );
+        return commandline;
+    }
+
+    protected Commandline createMakeWorkspaceCommandLine( AccuRevScmProviderRepository repository,
+                                                          ScmFileSet fileSet, ScmVersion version )
+    {
+        //TODO Implement support of fileSet, version if applyable
+        Commandline cmd = new Commandline();
+        cmd.setExecutable( this.accuRevExecutable );
+        //Append command name
+        cmd.addArguments( new String[]{"mkws"} );
+        //Append host param if needed
+        List params = new ArrayList();
+        AccuRevScmProvider.appendHostToParamsIfNeeded( repository, params );
+        cmd.addArguments( (String[]) params.toArray( new String[params.size()] ) );
+        //Append command arguments
+        cmd.addArguments( new String[]{
+            "-w", repository.getWorkspaceName(),
+            "-b", repository.getStreamName(),
+            "-l", fileSet.getBasedir().getAbsolutePath()
+        } );
+        return cmd;
+    }
+}
