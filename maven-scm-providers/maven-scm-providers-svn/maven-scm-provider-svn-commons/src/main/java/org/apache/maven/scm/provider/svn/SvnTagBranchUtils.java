d0f938b83496fbceb326aaaccc4d7e6e4787d41b
Vincent Siveton <vsiveton@apache.org>
2008-08-19 12:02:05 +0000

    SCM-379: SCM URL with query transformed incorrectly on release:prepare
    o fixed queryString 
    o added test case
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/scm/trunk@687028 13f79535-47bb-0310-9956-ffa450edef68

--- maven-scm-providers/maven-scm-providers-svn/maven-scm-provider-svn-commons/src/main/java/org/apache/maven/scm/provider/svn/SvnTagBranchUtils.java
+++ maven-scm-providers/maven-scm-providers-svn/maven-scm-provider-svn-commons/src/main/java/org/apache/maven/scm/provider/svn/SvnTagBranchUtils.java
@@ -210,6 +210,8 @@ public class SvnTagBranchUtils
         if ( repositoryUrl.indexOf( "?" ) >= 0 )
         {
             queryString = repositoryUrl.substring( repositoryUrl.indexOf( "?" ) );
+            // if repositoryUrl contains a query string, remove it from repositoryUrlRoot; will be re-appended later
+            projectRoot = StringUtils.replace( projectRoot, queryString, "" );
         }
 
         if ( branchTagName.indexOf( "://" ) >= 0 )
