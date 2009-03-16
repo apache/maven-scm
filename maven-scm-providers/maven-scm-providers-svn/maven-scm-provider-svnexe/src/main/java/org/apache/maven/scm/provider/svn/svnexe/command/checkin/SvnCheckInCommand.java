a6c9258e910ce9e0ed2373ce7f450008643155f8
Oliver Lamy <olamy@apache.org>
2009-03-16 22:09:29 +0000

    [SCM-448]  CheckInScmResult could return the scm revision (at least for svn provider)
    git-svn-id: https://svn.eu.apache.org/repos/asf/maven/scm/trunk@755019 13f79535-47bb-0310-9956-ffa450edef68

--- maven-scm-providers/maven-scm-providers-svn/maven-scm-provider-svnexe/src/main/java/org/apache/maven/scm/provider/svn/svnexe/command/checkin/SvnCheckInCommand.java
+++ maven-scm-providers/maven-scm-providers-svn/maven-scm-provider-svnexe/src/main/java/org/apache/maven/scm/provider/svn/svnexe/command/checkin/SvnCheckInCommand.java
@@ -106,7 +106,7 @@ public class SvnCheckInCommand
             return new CheckInScmResult( cl.toString(), "The svn command failed.", stderr.getOutput(), false );
         }
 
-        return new CheckInScmResult( cl.toString(), consumer.getCheckedInFiles() );
+        return new CheckInScmResult( cl.toString(), consumer.getCheckedInFiles(), Integer.toString( consumer.getRevision() ) );
     }
 
     // ----------------------------------------------------------------------
