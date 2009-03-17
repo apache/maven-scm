4d708c76982830421372d36dfb6a61ca42e53b97
Oliver Lamy <olamy@apache.org>
2009-03-17 22:19:42 +0000

    add unit for svn remote tagging cli generation
    git-svn-id: https://svn.apache.org/repos/asf/maven/scm/trunk@755423 13f79535-47bb-0310-9956-ffa450edef68

--- maven-scm-providers/maven-scm-providers-svn/maven-scm-provider-svnexe/src/test/java/org/apache/maven/scm/provider/svn/svnexe/command/tag/SvnExeTagCommandTckTest.java
+++ maven-scm-providers/maven-scm-providers-svn/maven-scm-provider-svnexe/src/test/java/org/apache/maven/scm/provider/svn/svnexe/command/tag/SvnExeTagCommandTckTest.java
@@ -19,12 +19,13 @@ package org.apache.maven.scm.provider.svn.svnexe.command.tag;
  * under the License.
  */
 
+import java.io.File;
+
+import org.apache.maven.scm.ScmTagParameters;
 import org.apache.maven.scm.provider.svn.command.tag.SvnTagCommandTckTest;
 import org.apache.maven.scm.provider.svn.repository.SvnScmProviderRepository;
 import org.apache.maven.scm.repository.ScmRepository;
 import org.codehaus.plexus.util.cli.Commandline;
-
-import java.io.File;
 
 /**
  * This test tests the tag command.
@@ -43,10 +44,38 @@ public class SvnExeTagCommandTckTest
 
         testCommandLine( "scm:svn:svn+ssh://foo.com/svn/trunk", "svntag", messageFile, "user",
                          "svn --username user --non-interactive copy --file " + messageFile.getAbsolutePath() +
-                             " . svn+ssh://user@foo.com/svn/tags/svntag" );
+                             " . svn+ssh://user@foo.com/svn/tags/svntag", null );
     }
+    
+    public void testTagRemoteTagHttps()
+        throws Exception
+    {
+        File messageFile = File.createTempFile( "maven-scm", "commit" );
+        messageFile.deleteOnExit();
 
-    private void testCommandLine( String scmUrl, String tag, File messageFile, String user, String commandLine )
+        ScmTagParameters scmTagParameters = new ScmTagParameters();
+        scmTagParameters.setRemoteTagging( true );
+        testCommandLine( "scm:svn:https://foo.com/svn/trunk", "svntag", messageFile, "user",
+                         "svn --username user --non-interactive copy --file " + messageFile.getAbsolutePath()
+                             + " https://foo.com/svn/trunk https://foo.com/svn/tags/svntag", scmTagParameters );
+    }    
+    
+    public void testTagRemoteTagHttpsWithRevision()
+    throws Exception
+{
+    File messageFile = File.createTempFile( "maven-scm", "commit" );
+    messageFile.deleteOnExit();
+
+    ScmTagParameters scmTagParameters = new ScmTagParameters();
+    scmTagParameters.setRemoteTagging( true );
+    scmTagParameters.setScmRevision( "12" );
+    testCommandLine( "scm:svn:https://foo.com/svn/trunk", "svntag", messageFile, "user",
+                     "svn --username user --non-interactive copy --file " + messageFile.getAbsolutePath()
+                         + " --revision 12 https://foo.com/svn/trunk https://foo.com/svn/tags/svntag", scmTagParameters );
+}    
+
+    private void testCommandLine( String scmUrl, String tag, File messageFile, String user, String commandLine,
+                                  ScmTagParameters scmTagParameters )
         throws Exception
     {
         File workingDirectory = getTestFile( "target/svn-update-command-test" );
@@ -57,8 +86,17 @@ public class SvnExeTagCommandTckTest
 
         svnRepository.setUser( user );
 
-        Commandline cl = SvnTagCommand.createCommandLine( svnRepository, workingDirectory, tag, messageFile );
+        Commandline cl = null;
 
+        if ( scmTagParameters == null )
+        {
+            cl = SvnTagCommand.createCommandLine( svnRepository, workingDirectory, tag, messageFile );
+        }
+        else
+        {
+            cl = SvnTagCommand.createCommandLine( svnRepository, workingDirectory, tag, messageFile, scmTagParameters );
+        }
+        System.out.println(" command " + cl.toString() );
         assertCommandLine( commandLine, workingDirectory, cl );
     }
 }
