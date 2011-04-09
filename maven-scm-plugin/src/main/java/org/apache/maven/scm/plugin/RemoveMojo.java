9511324ffeecbf200c932fc9a066815e5676b413
Olivier Lamy <olamy@apache.org>
2011-04-09 08:28:14 +0000

    [SCM-615] RemoveMojo to allow removing resources from version control. Submitted by Paul Mackinlay.
    git-svn-id: https://svn.apache.org/repos/asf/maven/scm/trunk@1090542 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-scm-plugin/src/main/java/org/apache/maven/scm/plugin/RemoveMojo.java
@@ -0,0 +1,60 @@
+package org.apache.maven.scm.plugin;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.scm.ScmException;
+import org.apache.maven.scm.command.remove.RemoveScmResult;
+import org.apache.maven.scm.repository.ScmRepository;
+
+import java.io.IOException;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Mark a set of files for deletion.
+ *
+ * @author <a href="paul@webotech.co.uk">Paul Mackinlay</a>
+ * @goal remove
+ * @aggregator
+ */
+public class RemoveMojo extends AbstractScmMojo {
+
+    /**
+     * The commit message.
+     *
+     * @parameter expression="${message}"
+     */
+    private String message;
+
+    /**
+     * {@inheritDoc}
+     */
+    public void execute() throws MojoExecutionException {
+        super.execute();
+        try {
+            ScmRepository repository = getScmRepository();
+            RemoveScmResult result = getScmManager().remove(repository, getFileSet(), message);
+            checkResult(result);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Cannot run remove command : ", e);
+        } catch (ScmException e) {
+            throw new MojoExecutionException("Cannot run remove command : ", e);
+        }
+    }
+}
---
+++ maven-scm-plugin/src/test/java/org/apache/maven/scm/plugin/RemoveMojoTest.java
@@ -0,0 +1,84 @@
+package org.apache.maven.scm.plugin;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.codehaus.plexus.util.StringUtils;
+
+import java.io.File;
+
+/**
+ * @author <a href="paul@webotech.co.uk">Paul Mackinlay</a>
+ */
+public class RemoveMojoTest extends AbstractMojoTestCase {
+
+    public void testShouldInvokeP4Delete() throws Exception {
+        String testConfig = "src/test/resources/mojos/remove/removeWithPerforce.xml";
+        try {
+            RemoveMojo removeMojo = (RemoveMojo) lookupMojo("remove", getTestFile(testConfig));
+            String connectionUrl = removeMojo.getConnectionUrl();
+            connectionUrl = StringUtils.replace(connectionUrl, "${basedir}", getBasedir());
+            connectionUrl = StringUtils.replace(connectionUrl, "\\", "/");
+            removeMojo.setWorkingDirectory(new File(getBasedir()));
+            removeMojo.setConnectionUrl(connectionUrl);
+
+            removeMojo.execute();
+        } finally {
+            // Just to be sure unedit anything that has been marked for delete
+            UnEditMojo unEditMojo = (UnEditMojo) lookupMojo("unedit", getTestFile(testConfig));
+            String connectionUrl = unEditMojo.getConnectionUrl();
+            connectionUrl = StringUtils.replace(connectionUrl, "${basedir}", getBasedir());
+            connectionUrl = StringUtils.replace(connectionUrl, "\\", "/");
+            unEditMojo.setWorkingDirectory(new File(getBasedir()));
+            unEditMojo.setConnectionUrl(connectionUrl);
+            unEditMojo.execute();
+        }
+    }
+
+    public void testShouldFailToInvokeP4Delete() throws Exception {
+        String testConfig = "src/test/resources/mojos/remove/removeWithPerforceNoIncludes.xml";
+        try {
+            RemoveMojo removeMojo = (RemoveMojo) lookupMojo("remove", getTestFile(testConfig));
+            String connectionUrl = removeMojo.getConnectionUrl();
+            connectionUrl = StringUtils.replace(connectionUrl, "${basedir}", getBasedir());
+            connectionUrl = StringUtils.replace(connectionUrl, "\\", "/");
+            removeMojo.setWorkingDirectory(new File(getBasedir()));
+            removeMojo.setConnectionUrl(connectionUrl);
+
+            try {
+                removeMojo.execute();
+                fail("At least one file needs to be included for removal");
+            } catch (MojoExecutionException e) {
+                // we're expecting this exception
+            }
+        } finally {
+            // Just to be sure unedit anything that has been marked for delete
+            UnEditMojo unEditMojo = (UnEditMojo) lookupMojo("unedit", getTestFile(testConfig));
+            String connectionUrl = unEditMojo.getConnectionUrl();
+            connectionUrl = StringUtils.replace(connectionUrl, "${basedir}", getBasedir());
+            connectionUrl = StringUtils.replace(connectionUrl, "\\", "/");
+            unEditMojo.setWorkingDirectory(new File(getBasedir()));
+            unEditMojo.setConnectionUrl(connectionUrl);
+            unEditMojo.execute();
+        }
+    }
+
+}
