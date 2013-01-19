b84f2aa76aae96f70fdca51a2e11a4a5233dd428
rfscholte <rfscholte@apache.org>
2013-01-19 17:12:35 +0100

    Use System.clearProperty instead of System.getProperties().remove

--- maven-scm-providers/maven-scm-provider-perforce/src/test/java/org/apache/maven/scm/provider/perforce/command/changelog/PerforceChangeLogCommandTest.java
+++ maven-scm-providers/maven-scm-provider-perforce/src/test/java/org/apache/maven/scm/provider/perforce/command/changelog/PerforceChangeLogCommandTest.java
@@ -48,7 +48,7 @@ public class PerforceChangeLogCommandTest
         throws Exception
     {
         // Some tests don't expect this property, so when tests are executed in random order these might fail
-        System.getProperties().remove( PerforceScmProvider.DEFAULT_CLIENTSPEC_PROPERTY );
+        System.clearProperty( PerforceScmProvider.DEFAULT_CLIENTSPEC_PROPERTY );
     }
     
     public void testGetCommandLine()
