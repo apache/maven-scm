8f45a26bc4adaa86b7af77fd691a525e93d4d901
imod <domi@fortysix.ch>
2013-11-21 20:40:23 +0100

    revert workaround for windows

--- maven-scm-providers/maven-scm-providers-git/maven-scm-provider-jgit/src/main/java/org/apache/maven/scm/provider/git/jgit/command/JGitUtils.java
+++ maven-scm-providers/maven-scm-providers-git/maven-scm-provider-jgit/src/main/java/org/apache/maven/scm/provider/git/jgit/command/JGitUtils.java
@@ -81,16 +81,6 @@ import java.util.Set;
 public class JGitUtils
 {
     
-    private static boolean IS_WINDOWS = false;
-    static
-    {
-        String osName = System.getProperty( "os.name" );
-        if ( osName.indexOf( "windows" ) != -1 )
-        {
-             IS_WINDOWS = true;
-        }
-    }
-
     private JGitUtils()
     {
         // no op
@@ -105,14 +95,6 @@ public class JGitUtils
         if ( git != null && git.getRepository() != null )
         {
             git.getRepository().close();
-            if ( IS_WINDOWS )
-            {
-                // Since memory mapping is controlled by the GC we need to
-                // tell it this is a good time to clean up and unlock
-                // memory mapped files.
-                // see also org.eclipse.jgit.junit.LocalDiskRepositoryTestCase
-                System.gc();
-            }
         }
     }
 
