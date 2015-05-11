apache/maven-scm

commit 2d7502077559d46f8009e45311b8ce5ab79f01bb

[SCM-795] Fixed. Content on stderr will no longer raise an exception.

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/checkin/JazzCheckInCommand.java
@@ -108,7 +108,7 @@ public class JazzCheckInCommand
         ErrorConsumer errConsumer = new ErrorConsumer( getLogger() );
 
         int status = createChangesetCmd.execute( outputConsumer, errConsumer );
-        if ( status != 0 || errConsumer.hasBeenFed() )
+        if ( status != 0 )
         {
             return new CheckInScmResult( createChangesetCmd.getCommandString(),
                                          "Error code for Jazz SCM create changeset command - " + status,
@@ -139,7 +139,7 @@ public class JazzCheckInCommand
             ErrorConsumer errConsumer = new ErrorConsumer( getLogger() );
 
             int status = deliverCmd.execute( deliverConsumer, errConsumer );
-            if ( status != 0 || errConsumer.hasBeenFed() )
+            if ( status != 0 )
             {
                 return new CheckInScmResult( deliverCmd.getCommandString(),
                                              "Error code for Jazz SCM deliver command - " + status,

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/update/JazzUpdateCommand.java
@@ -69,12 +69,12 @@ public class JazzUpdateCommand
         }
 
         JazzUpdateConsumer updateConsumer = new JazzUpdateConsumer( repo, getLogger() );
-        ErrorConsumer err = new ErrorConsumer( getLogger() );
+        ErrorConsumer errConsumer = new ErrorConsumer( getLogger() );
 
         JazzScmCommand updateCmd = createAcceptCommand( repo, fileSet );
-        int status = updateCmd.execute( updateConsumer, err );
+        int status = updateCmd.execute( updateConsumer, errConsumer );
 
-        if ( status != 0 || err.hasBeenFed() )
+        if ( status != 0 )
         {
             return new UpdateScmResult( updateCmd.getCommandString(),
                                         "Error code for Jazz SCM update command - " + status, err.getOutput(), false );

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/add/JazzAddCommand.java
@@ -155,7 +155,7 @@ public class JazzAddCommand
         JazzScmCommand command = createAddCommand( repo, fileSet );
 
         int status = command.execute( addConsumer, errConsumer );
-        if ( status != 0 || errConsumer.hasBeenFed() )
+        if ( status != 0 )
         {
             return new AddScmResult( command.getCommandString(),
                                      "Error code for Jazz SCM add (checkin) command - " + status,

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/blame/JazzBlameCommand.java
@@ -71,7 +71,7 @@ public class JazzBlameCommand
         ErrorConsumer errConsumer = new ErrorConsumer( getLogger() );
 
         int status = blameCmd.execute( blameConsumer, errConsumer );
-        if ( status != 0 || errConsumer.hasBeenFed() )
+        if ( status != 0 )
         {
             return new BlameScmResult( blameCmd.getCommandString(), "Error code for Jazz SCM blame command - " + status,
                                        errConsumer.getOutput(), false );

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/changelog/JazzChangeLogCommand.java
@@ -93,7 +93,7 @@ public class JazzChangeLogCommand
         JazzHistoryConsumer changeLogConsumer = new JazzHistoryConsumer( repo, getLogger(), changeSets );
         ErrorConsumer errConsumer = new ErrorConsumer( getLogger() );
         int status = historyCommand.execute( changeLogConsumer, errConsumer );
-        if ( status != 0 || errConsumer.hasBeenFed() )
+        if ( status != 0 )
         {
             return new ChangeLogScmResult( historyCommand.getCommandString(),
                                            "Error code for Jazz SCM history command - " + status,
@@ -106,7 +106,7 @@ public class JazzChangeLogCommand
             new JazzListChangesetConsumer( repo, getLogger(), changeSets, datePattern );
         errConsumer = new ErrorConsumer( getLogger() );
         status = listChangesetsCommand.execute( listChangesetConsumer, errConsumer );
-        if ( status != 0 || errConsumer.hasBeenFed() )
+        if ( status != 0 )
         {
             return new ChangeLogScmResult( listChangesetsCommand.getCommandString(),
                                            "Error code for Jazz SCM list changesets command - " + status,

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/checkout/JazzCheckOutCommand.java
@@ -72,7 +72,7 @@ public class JazzCheckOutCommand
         ErrorConsumer errConsumer = new ErrorConsumer( getLogger() );
 
         int status = checkoutCmd.execute( checkoutConsumer, errConsumer );
-        if ( status != 0 || errConsumer.hasBeenFed() )
+        if ( status != 0 )
         {
             return new CheckOutScmResult( checkoutCmd.getCommandString(),
                                           "Error code for Jazz SCM checkout (load) command - " + status,

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/diff/JazzDiffCommand.java
@@ -138,7 +138,7 @@ public class JazzDiffCommand
                 ErrorConsumer errConsumer = new ErrorConsumer( getLogger() );
                 diffCmd = createDiffCommand( repo, fileSet, relativePath );
                 int status = diffCmd.execute( diffConsumer, errConsumer );
-                if ( status != 0 || errConsumer.hasBeenFed() )
+                if ( status != 0 )
                 {
                     // Return a false result (not the usual SCMResult)
                     return new DiffScmResult( diffCmd.toString(), "The scm diff command failed.",

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/edit/JazzEditCommand.java
@@ -78,7 +78,7 @@ public class JazzEditCommand
         JazzScmCommand editCmd = createEditCommand( repo, fileSet );
         int status = editCmd.execute( editConsumer, errConsumer );
 
-        if ( status != 0 || errConsumer.hasBeenFed() )
+        if ( status != 0 )
         {
             return new EditScmResult( editCmd.getCommandString(), "Error code for Jazz SCM edit command - " + status,
                                       errConsumer.getOutput(), false );

commit db8a2703334a02283cc407bb2f708ebd8719bcaf

Syntax error introduced by 2d7502077559d46f8009e45311b8ce5ab79f01bb Removed forgotten stderr consumer validation

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/checkin/JazzCheckInCommand.java
@@ -142,7 +142,7 @@ public class JazzCheckInCommand
                     errConsumer = new ErrorConsumer( getLogger() );
         
                     status = changesetAssociateCmd.execute( outputConsumer, errConsumer );
-                    if ( status != 0 || errConsumer.hasBeenFed() )
+                    if ( status != 0  )
                     {
                         return new CheckInScmResult( changesetAssociateCmd.getCommandString(),
                                                      "Error code for Jazz SCM changeset associate command - " + status,

maven-scm-providers/maven-scm-provider-jazz/src/main/java/org/apache/maven/scm/provider/jazz/command/update/JazzUpdateCommand.java
@@ -77,7 +77,8 @@ public class JazzUpdateCommand
         if ( status != 0 )
         {
             return new UpdateScmResult( updateCmd.getCommandString(),
-                                        "Error code for Jazz SCM update command - " + status, err.getOutput(), false );
+                                        "Error code for Jazz SCM update command - " + status, 
+                                        errConsumer.getOutput(), false );
         }
 
         if ( getLogger().isDebugEnabled() )