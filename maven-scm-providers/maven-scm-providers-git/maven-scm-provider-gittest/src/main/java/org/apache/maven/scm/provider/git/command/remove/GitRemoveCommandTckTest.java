b1815b5e0419d2636c3ea0ee7ad1856ade6f9fa5
Michael Osipov <michaelo@apache.org>
2022-07-22 11:44:04 +0200

    Simplify

--- maven-scm-providers/maven-scm-providers-git/maven-scm-provider-gittest/src/main/java/org/apache/maven/scm/provider/git/command/remove/GitRemoveCommandTckTest.java
+++ maven-scm-providers/maven-scm-providers-git/maven-scm-provider-gittest/src/main/java/org/apache/maven/scm/provider/git/command/remove/GitRemoveCommandTckTest.java
@@ -20,7 +20,6 @@ package org.apache.maven.scm.provider.git.command.remove;
  */
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
@@ -30,7 +29,6 @@ import org.apache.maven.scm.ScmFileSet;
 import org.apache.maven.scm.ScmTckTestCase;
 import org.apache.maven.scm.command.remove.RemoveScmResult;
 import org.apache.maven.scm.provider.git.GitScmTestUtils;
-import org.codehaus.plexus.util.FileUtils;
 
 /**
  * This test tests the remove command.
@@ -50,9 +48,7 @@ public abstract class GitRemoveCommandTckTest
     public void testCommandRemoveWithFile()
         throws Exception
     {
-        File workingDirectory = createTempDirectory();
-
-        File toBeRemoved = new File( workingDirectory.getAbsolutePath() + File.separator + "toto.xml" );
+        File toBeRemoved = new File( getWorkingDirectory().getAbsolutePath() + File.separator + "toto.xml" );
 
         Files.write( toBeRemoved.getAbsoluteFile().toPath(),
             "data".getBytes( StandardCharsets.UTF_8 ), StandardOpenOption.APPEND, StandardOpenOption.CREATE );
@@ -60,48 +56,30 @@ public abstract class GitRemoveCommandTckTest
         ScmFileSet fileSet =  new ScmFileSet( getWorkingCopy(), toBeRemoved );
         RemoveScmResult removeResult = getScmManager().remove( getScmRepository(), fileSet, getBasedir() );
         assertResultIsSuccess( removeResult );
-
-        FileUtils.deleteDirectory( workingDirectory );
     }
 
     public void testCommandRemoveWithDirectory()
         throws Exception
     {
-        File workingDirectory = createTempDirectory();
-
-        File toBeRemoved = new File( workingDirectory.getAbsolutePath() + File.separator + "toto" );
+        File toBeRemoved = new File( getWorkingDirectory().getAbsolutePath() + File.separator + "toto" );
         toBeRemoved.mkdir();
 
         ScmFileSet fileSet =  new ScmFileSet( getWorkingCopy(), toBeRemoved );
         RemoveScmResult removeResult = getScmManager().remove( getScmRepository(), fileSet, getBasedir() );
         assertResultIsSuccess( removeResult );
-
-        FileUtils.deleteDirectory( workingDirectory );
     }
 
     public void testCommandRemoveWithTwoDirectories()
         throws Exception
     {
-        File workingDirectory = createTempDirectory();
-
-        File toBeRemoved1 = new File( workingDirectory.getAbsolutePath() + File.separator + "toto" );
+        File toBeRemoved1 = new File( getWorkingDirectory().getAbsolutePath() + File.separator + "toto" );
         toBeRemoved1.mkdir();
 
-        File toBeRemoved2 = new File( workingDirectory.getAbsolutePath() + File.separator + "tata" );
+        File toBeRemoved2 = new File( getWorkingDirectory().getAbsolutePath() + File.separator + "tata" );
         toBeRemoved2.mkdir();
 
         ScmFileSet fileSet =  new ScmFileSet( getWorkingCopy(), Arrays.asList( toBeRemoved1, toBeRemoved2 ) );
         RemoveScmResult removeResult = getScmManager().remove( getScmRepository(), fileSet, getBasedir() );
         assertResultIsSuccess( removeResult );
-        FileUtils.deleteDirectory( workingDirectory );
-    }
-
-    private File createTempDirectory()
-        throws IOException
-    {
-        File dir = File.createTempFile( "gitexe", "test" );
-        dir.delete();
-        dir.mkdir();
-        return dir;
     }
 }
