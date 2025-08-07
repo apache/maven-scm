3b8a27b8177db3a7ab4005e361470f0c45310d9c
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2025-08-07 06:17:28 +0000

    Replace unintended octal constants (#1287)

--- maven-scm-test/src/main/java/org/apache/maven/scm/tck/command/info/InfoCommandTckTest.java
+++ maven-scm-test/src/main/java/org/apache/maven/scm/tck/command/info/InfoCommandTckTest.java
@@ -47,7 +47,7 @@ public abstract class InfoCommandTckTest extends ScmTckTestCase {
         assertEquals("Mark Struberg <struberg@yahoo.de>", item.getLastChangedAuthor());
         assertEquals("92f139dfec4d1dfb79c3cd2f94e83bf13129668b", item.getRevision());
         assertEquals(
-                OffsetDateTime.of(2009, 03, 15, 19, 14, 02, 0, ZoneOffset.ofHours(1)), item.getLastChangedDateTime());
+                OffsetDateTime.of(2009, 3, 15, 19, 14, 2, 0, ZoneOffset.ofHours(1)), item.getLastChangedDateTime());
     }
 
     @Test
@@ -61,6 +61,6 @@ public abstract class InfoCommandTckTest extends ScmTckTestCase {
         assertEquals("Mark Struberg <struberg@yahoo.de>", item.getLastChangedAuthor());
         assertEquals("92f139dfec4d1dfb79c3cd2f94e83bf13129668b", item.getRevision());
         assertEquals(
-                OffsetDateTime.of(2009, 03, 15, 19, 14, 02, 0, ZoneOffset.ofHours(1)), item.getLastChangedDateTime());
+                OffsetDateTime.of(2009, 3, 15, 19, 14, 2, 0, ZoneOffset.ofHours(1)), item.getLastChangedDateTime());
     }
 }
